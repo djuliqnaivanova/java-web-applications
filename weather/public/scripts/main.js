6bd593d66066f9206bf34895f0716e1e24b1361f
djuliqnaivanova <djuliqna_ivanova@abv.bg>
2017-05-21 10:30:38 +0300

    fix

--- weather/public/scripts/main.js
+++ weather/public/scripts/main.js
@@ -1,50 +1,37 @@
 const url = "http://localhost:3001/api/forecast/";
-let $notifier = $("#notifier")
+let $notifier = $("#notifier");
+let $forecastConteiner = $('#forecast-conteiner');
 
-$("#btn-forecast").click(()=>{
+$("#btn-forecast").click(() => {
     let town = $('#input-town').val();
+    $notifier.html('Getting your forecast!');
+    $forecastConteiner.html("");
 
     if (town.length < 1) {
         $notifier.html("Please choice a town!");
 
-        setTimeout(()=>{
-        $notifier.html("");
+        setTimeout(() => {
+            $notifier.html("");
         }, 5000);
 
         return;
     }
 
-    let $forecastConteiner = $('#forecast-conteiner')
 
     $.ajax({
-        url:url+town
+        url: url + town
     }).then(res => {
-        $forecastConteiner.append(`<h3>Town: ${res.location.name}</h3>`)
-        $forecastConteiner.append(`<div>Country: ${res.location.country}</div>`)
+        $notifier.html("");
 
-        $forecastConteiner.append(`<div>Celsius: ${res.current.temp_c}</div>`)
-        $forecastConteiner.append(`<div>Condition: ${res.current.condition.text}</div>`)
-        
-        $forecastConteiner.append(`<img src="${res.current.condition.icon.trimLeft('/')}" class="img-thumbnail"/>`)
+        $forecastConteiner.append(`<h3>Town: ${res.location.name}</h3>`);
+        $forecastConteiner.append(`<div>Country: ${res.location.country}</div>`);
 
-        
-        $forecastConteiner.append(`<div>Wind: ${res.current.wind_kph}</div>`)
+        $forecastConteiner.append(`<div>Celsius: ${res.current.temp_c}</div>`);
+        $forecastConteiner.append(`<div>Condition: ${res.current.condition.text}</div>`);
+
+        $forecastConteiner.append(`<img src="${res.current.condition.icon.trimLeft('/')}" class="img-thumbnail"/>`);
 
 
+        $forecastConteiner.append(`<div>Wind: ${res.current.wind_kph}</div>`);
     })
 })
-
-/**
- * location
- * --name
- * --country
- * current 
- * --temp_c
- * --condition
- * ----text
- * ----icon
- * --wind_kph
- */
-
-
-
